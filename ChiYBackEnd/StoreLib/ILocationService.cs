3c72af495a6bc0094dedcb702f78406d13b39fd1
angelagongli <angelagongli@gmail.com>
2020-11-16 07:21:20 -0600

    add repo service interface .cs files

---
+++ ChiYBackEnd/StoreLib/ILocationService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+using StoreDB.Models;
+
+namespace StoreLib
+{
+    public interface ILocationService
+    {
+        List<Location> GetAllLocations();
+
+        Location GetLocationById(int locationId);
+    }
+}
