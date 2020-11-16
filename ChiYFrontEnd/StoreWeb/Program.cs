d2297baa312c4bc74a9b74aaf22c68be8f91d73d
angelagongli <angelagongli@gmail.com>
2020-11-16 07:34:29 -0600

    add Program.cs

---
+++ ChiYFrontEnd/StoreWeb/Program.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace StoreWeb
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.File("log.txt")
+            .CreateLogger();
+
+            Log.Information("Program started");
+
+            CreateHostBuilder(args).Build().Run();
+
+            Log.CloseAndFlush();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
